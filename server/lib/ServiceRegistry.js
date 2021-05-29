f7e4ce46702164179fc3d413bd9fd33e905723e8
abdulloooh <abdullahakinwumi@gmail.com>
2021-05-29 18:53:58 +0100

    registry clean up

--- server/lib/ServiceRegistry.js
+++ server/lib/ServiceRegistry.js
@@ -4,18 +4,20 @@ export default class ServiceRegistry {
   constructor(log) {
     this.log = log;
     this.services = {};
-    this.timeout = 30;
+    this.timeout = 15;
   }
 
   get({ name, version }) {
+    Promise.resolve(this.cleanup());
     const candidates = Object.values(this.services).filter(
-      (c) => c.name === name && satisfies(c.version, version)
+      (service) => service.name === name && satisfies(service.version, version)
     );
 
     return candidates[Math.floor(Math.random() * candidates.length)];
   }
 
   register({ name, version, ip, port }) {
+    Promise.resolve(this.cleanup());
     const key = name + version + ip + port;
 
     if (!this.services[key]) {
@@ -40,4 +42,23 @@ export default class ServiceRegistry {
     if (this.services[key]) delete this.services[key];
     return key;
   }
+
+  async cleanup() {
+    const now = Math.floor(new Date() / 1000);
+
+    Object.keys(this.services).map((key) => {
+      if (this.services[key].timestamp + this.timeout < now) {
+        delete this.services[key];
+        this.log.debug(`Removed service ${key}`);
+      }
+    });
+  }
 }
+
+/**
+ * So, the assumption is every service re-registers itself at a specified interval
+ * in order to monitor service availability.
+ *
+ * So, there should be a cleanup function that frequently check for any service not noticed
+ * after expected ping interval i.e likely shut down services and deregister them.
+ */
